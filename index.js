274929f2e1c32c8155796f17421c9a3a3f9bc879
ronaim <44949408+ronaim@users.noreply.github.com>
2020-01-28 12:22:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,214 @@
 
-const predictions = `5601 - 5650             bitcoincidence
-6301 - 6350             Spray.
-7351 - 7400             Chris601
-7001 - 7050             Koal-84
-7251 - 7300             haloxon
-7301 - 7350             mole0815
-7651 - 7700             xyz
-7951 - 8000             tyKiwanuka
-8001 - 8050             RaspoBTC
-8051 - 8100             bct_ail
-8151 - 8200             Nestade
-8501 - 8550             hanspeter77
-9201 - 9250             Ov3R
-9901 - 9950             1miau
-10001 - 10050         Real-Duke
-10701 - 10750         micgoossens
-11001 - 11050         Lakai
-11401 - 11450         butch3r
-11750 - 11800         Lafu
-12501 - 12550         cygan
-13451 - 13500         Buchi-88
-13601 - 13650         efialtis
-14951 - 15000         Unknown01
-15001 - 15050         MishaMuc
-16501 - 16550         o_solo_miner
-18831 - 18.880        MinoRaiola
-19001 - 19050         Alabasta
-55201 - 55250         fsm247`;
+const predictions = `3101-3150          Yaplatu
+3151-3200          KTChampions *
+3451-3500          Eduarop
+4401-4450          rdbase
+4451-4500          MagicByt3
+5051-5100          fabiorem
+5151-5200          sgbett *
+5451-5500          cabalism13
+5601-5650          Asuspawer09
+5751-5800          Mallyx
+5801-5850          makrospex
+5901-5950          Mbitr
+5951-6000          Snork1979
+6001-6050          tyKiwanuka *
+6051-6100          tweetious
+6151-6200          bakasabo
+6201-6250          dothebeats *
+6251-6300          Heisenberg_Hunter *
+6301-6350          Spray. *
+6351-6400          DoublerHunter
+6401-6450          Saint-loup
+6451-6500          Icygreen *
+6501-6550          haloxon
+6551-6600          Majormax *
+6601-6650          bitebits
+6651-6700          Corrosive
+6701-6750          Polar91
+6751-6800          theymos *
+6801-6850          bbc.reporter *
+6851-6900          ChuckBuck
+6901-6950          Biodom
+6951-7000          Last of the V8s
+7001-7050          tvplus006
+7051-7100          sirazimuth
+7101-7150          GazetaBitcoin
+7151-7200          unsoindovo
+7201-7250          hosseinimr93 *
+7251-7300          wheelz1200
+7301-7350          dragonvslinux *
+7351-7400          Cryptotourist
+7401-7450          android17
+7451-7500          bct_ail
+7501-7550          iasenko
+7551-7600          xyz
+7601-7650          sheenshane
+7651-7700          7jaka7
+7701-7750          Lauda
+7751-7800          ezeminer *
+7801-7850          Globb0 *
+7851-7900          teeGUMES
+7901-7950          tg88
+7951-8000          leea-1334 *
+8001-8050          HairyMaclairy *
+8051-8100          wachtwoord
+8101-8150          brobbel
+8151-8200          gentlemand
+8201-8250          Ryker1
+8251-8300          JSRAW
+8301-8350          marlboroza
+8351-8400          efialtis
+8401-8450          TopTort777
+8451-8500          madnessteat
+8501-8550          pawel7777
+8551-8600          kurious  *
+8601-8650          oscar2000 *
+8651-8700          mindrust
+8701-8750          Phil_S *
+8751-8800          abel1337
+8801-8850          malevolent *
+8851-8900          mu_enrico
+8901-8950          stompix
+8951-9000          Mitchell
+9001-9050          Dunkelheit667 *
+9051-9100          actmyname
+9101-9150          Olegya199
+9151-9200          somac.
+9201-9250          psycodad
+9251-9300          Tytanowy Janusz
+9301-9350          hatshepsut93 *
+9351-9400          BobLawblaw
+9401-9450          Febo
+9451-9500          buwaytress *
+9501-9550          FutureBitcoin
+9551-9600          Vispilio
+9601-9650          1miau
+9651-9700          DragonDance
+9701-9750          JimboToronto *
+9751-9800          AlcoHoDL
+9801-9850          wwzsocki
+9851-9900          thisisntbic
+9901-9950          DeathAngel
+9951-10000        GrosWesh
+10001-10050      jojo69
+10051-10100      Pamoldar
+10101-10150      Lanatsa
+10151-10200      krogothmanhattan
+10201-10250      vapourminer
+10251-10300      JanEmil
+10301-10350      BitcoinPenny
+10351-10400      cryptobaboon
+10401-10450      RapTarX
+10451-10500      ivomm *
+10501-10550      DireWolfM14
+10551-10600      F2b
+10601-10650      hacker1001101001
+10651-10700      LoyceV *
+10701-10750      xLays
+10751-10800      d_eddie
+10801-10850      SPQRCoin
+10851-10900      akhjob
+10901-10950      Dabs
+10951-11000      wh1rlw1nd *
+11001-11050      RaspoBTC
+11051-11100      Swordsoffreedom *
+11101-11150      slaman29 *
+11151-11200      Searing
+11201-11250      FontSeli
+11251-11300      LFC_Bitcoin
+11301-11350      Real-Duke
+11351-11400      geophphreigh
+11401-11450      Sat0shisGhost
+11451-11500      bitmover
+11501-11550      LUCKMCFLY *
+11551-11600      alegotardo
+11601-11650      STT *
+11651-11700      bitserve
+11701-11750      johhnyUA *
+11751-11800      xenon131
+11801-11850      freedomgo
+11851-11900      Kasabus
+11901-11950      P_Shep
+11951-12000      WhiteManWhite
+12001-12050      Stedsm
+12051-12100      Soonandwaite
+12101-12150      naska21
+12151-12200      gokudera
+12201-12250      Lesbian Cow
+12251-12300      Darker45 *
+12301-12350      o_e_l_e_o
+12351-12400      Lakai01
+12401-12450      NeuroticFish
+12451-12500      cygan
+12501-12550      ChinkyEyes
+12551-12600      Questat
+12601-12650      tokeweed
+12651-12700      exstasie
+12701-12750      rhomelmabini
+12751-12800      Russlenat
+12801-12850      Nestade
+12851-12900      asu
+12901-12950      figmentofmyass
+13001-13050      Hhampuz
+13051-13100      jokers10
+13101-13150      hanspeter77
+13151-13200      Captain-Cryptory
+13201-13250      Yamifoud *
+13251-13300      SFR10
+13301-13350      JollyGood
+13351-13400      Yatsan
+13401-13450      eddie13
+13451-13500      Buchi-88
+13501-13550      epis11
+13551-13600      HI-TEC99
+13751-13800      squatter
+13951-14000      igor72
+14001-14050      klaaas
+14151-14200      Unknown01
+14201-14250      Hueristic
+14351-14400      Debonaire217
+14401-14450      mole0815
+14451-14500      Koal-84
+14501-14550      qwk
+14551-14600      Sanitough
+14601-14650      Viscore
+14651-14700      Lucasgabd
+14701-14750      fronti
+14751-14800      mirakal
+14901-14950      Steamtyme
+15001-15050      tranthidung
+15151-15200      zasad@
+15201-15250      TryNinja
+15251-15300      stadus
+16201-16250      plvbob0070
+16301-16350      Alai19 *
+16351-16400      xhomerx10
+16401-16450      finaleshot2016
+16501-16550      PoolMinor
+16601-16650      SwayStar123
+16801-16850      yefi
+16851-16900      Vaculin
+17001-17050      Paashaas
+17451-17500      friends1980
+17501-17550      minerjones
+17801-17850      HBKMusiK
+17951-18000      DdmrDdmr
+18801-18850      Wind_FURY
+18851-18900      rdluffy
+19501-19550      MishaMuc
+19651-19700      posi
+20001-20050      ChefImGartenPavillion
+21951-22000      bitcoinPsycho *
+22501-22550      Bitcoinaire
+22551-22600      bkbirge
+23201-23250      pressnegl
+23501-23550      Hyperjacked
+24501-24550      VB1001
+25001-25050      fillippone *
+28501-28550      Smartprofit
+34001-34050      smartcomet
+37001-37050      StartupAnalyst`;
 
 async function getPrice() {
     try {
