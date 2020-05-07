b23e3a88607a0551515f047154f35f4ee71632e6
ronaim <44949408+ronaim@users.noreply.github.com>
2020-05-07 16:17:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 
-const predictions = `4100 - 4199              Spray.
-6700 - 6799              haloxon
-7000 - 7099              Koal-84
-6100 - 6199              xyz
-6500 - 6599              1miau
-6600 - 6699              Souri
-7200 - 7299              BTC-Liz
-7400 - 7499              Real-Duke
-7800 – 7899              btc-ail
-8500 - 8599              hanspeter77
-9200 - 9299              Soonandwaite
-9600 - 9699              CoinEraser
-9800 - 9899              mole0815
-10000 - 10099          Unknown01
-10500 - 10599          Lakai01
-11100 - 11199          ChefImGartenPavillion
-12200 - 12299          bullrun2020bro
-12400 - 12499          cygan
-13400 - 13499          Buchi-88
-14400 - 14499          JL0
-15000 - 15099          MishaMuc
-16000 - 16099          o_solo_miner
-55200 - 55299          fsm247
-121200 - 121299       Ov3r`;
+const predictions = `4100 - 4199      Spray.
+6700 - 6799      haloxon
+7000 - 7099      Koal-84
+6100 - 6199      xyz
+6500 - 6599      1miau
+6600 - 6699      Souri
+7200 - 7299      BTC-Liz
+7400 - 7499      Real-Duke
+7800 – 7899      btc-ail
+8500 - 8599      hanspeter77
+9200 - 9299      Soonandwaite
+9600 - 9699      CoinEraser
+9800 - 9899      mole0815
+10000 - 10099      Unknown01
+10500 - 10599      Lakai01
+11100 - 11199      ChefImGartenPavillion
+12200 - 12299      bullrun2020bro
+12400 - 12499      cygan
+13400 - 13499      Buchi-88
+14400 - 14499      JL0
+15000 - 15099      MishaMuc
+16000 - 16099      o_solo_miner
+55200 - 55299      fsm247
+121200 - 121299      Ov3r`;
 
 async function getPrice() {
     try {
